e24424bd015a07c1a4bd37a81dc10f21f8bacbc5
Alejandro Velazquez <velzqueza@gmail.com>
2024-12-15 15:20:31 -0600

    Changed the game to use bitmap fonts to solve some placing issues. Made the game use just one image for both donkey sprites.

--- sketch.js
+++ sketch.js
@@ -16,14 +16,7 @@ var gameScreen = 1;
 var donkeyA, donkeyB, ball;
 var soundHit;
 var imgInit, imgField, imgOL;
-var textvs, textvs2, textvs3, texvs4, textvs5, textvs6;
-
-WebFontConfig = {
-    active: function() { game.time.events.add(Phaser.Timer.SECOND, createText, this); },
-    google: {
-      families: ['Press+Start+2P']
-    }
-};
+var textvs, textvs2, textvs3, texvs4, textvs5, textvs6, textBM;
 
 function restart(){
   score = 0;
@@ -42,7 +35,7 @@ function restart(){
 }
 
 function overlapDonkeyA(){
-  donkeyA.loadTexture("donkeyAk");
+  donkeyA.loadTexture("donkey_k");
   soundHit.play();
   kickedA = true;
   speed = game.rnd.integerInRange(250, 700);
@@ -51,7 +44,7 @@ function overlapDonkeyA(){
 }
 
 function overlapDonkeyB(){
-  donkeyB.loadTexture("donkeyBk");
+  donkeyB.loadTexture("donkey_k");
   soundHit.play();
   kickedB = true;
   speed = game.rnd.integerInRange(250, 700);
@@ -62,13 +55,10 @@ function overlapDonkeyB(){
 function createText(){
   game.stage.backgroundColor = "#71F3FF";
   imgInit = game.add.sprite(6, 7, "titlescreen");
-  textvs = game.add.text(90, 440, "press S or K\nto start");
-  textvs.font = "Press Start 2P";
-  textvs.fontSize = 25;
-  textvs.align = "center";
-  textvs.fill = "#FFA397";
-  textvs.stroke = "#DD5257";
-  textvs.strokeThickness = 8;
+  // Create a bitmap text object 
+  textvs = game.add.bitmapText(50, 450, 'PressStart2P', 'press S or K\n  to start');
+  // Apply a tint to the bitmap text
+  textvs.tint = 0xdd5555;
   soundHit = game.add.audio("ballHit");
 }
 
@@ -77,17 +67,16 @@ function preload() {
   game.load.image("titlescreen", "assets/titlescreen.png");
   game.load.image("field", "assets/field.png");
   game.load.image("ball", "assets/ball.png");
-  game.load.image("donkeyA", "assets/donkeyA.png");
-  game.load.image("donkeyB", "assets/donkeyB.png");
-  game.load.image("donkeyAk", "assets/donkeyAk.png");
-  game.load.image("donkeyBk", "assets/donkeyBk.png");
+  game.load.image("donkey", "assets/donkey.png");
+  game.load.image("donkey_k", "assets/donkey_k.png");
   game.load.audio("ballHit", ["assets/ballHit.mp3", "assets/ballHit.ogg"]);
-  game.load.script("webfont", "//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js");
+  game.load.bitmapFont('PressStart2P', 'assets/fonts/PressStart2P.png', 'assets/fonts/PressStart2P.xml');
 }
 
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
   game.stage.backgroundColor = "#222527";
+  createText();
 }
 
 function update () {
@@ -106,30 +95,18 @@ function update () {
         newScreen = false;
         textvs.destroy();
         imgInit.destroy();
-        textvs = game.add.text(400, 140, "Enter player name:");
-        textvs.anchor.setTo(0.5, 0.5);
-        textvs.font = "Press Start 2P";
-        textvs.fontSize = 25;
-        textvs.align = "center";
-        textvs.fill = "#FFA397";
-        textvs.stroke = "#DD5257";
-        textvs.strokeThickness = 8;
-        textvs2 = game.add.text(400, 295, playerName[0] + " " + playerName[1] + " " + playerName[2] + " " + playerName[3]);
-        textvs2.anchor.setTo(0.5, 0.5);
-        textvs2.font = "Press Start 2P";
-        textvs2.fontSize = 50;
-        textvs2.align = "center";
-        textvs2.fill = "#5FFFFD";
-        textvs2.stroke = "#00B9B5";
-        textvs2.strokeThickness = 8;
-        textvs3 = game.add.text(400, 460,"press S to choose a letter\npress K to go to next letter");
-        textvs3.anchor.setTo(0.5, 0.5);
-        textvs3.font = "Press Start 2P";
-        textvs3.fontSize = 25;
-        textvs3.align = "center";
-        textvs3.fill = "#FFA397";
-        textvs3.stroke = "#DD5257";
-        textvs3.strokeThickness = 8;
+        textvs = game.add.bitmapText(128, 128, 'PressStart2P', 'Enter player name');
+        textvs.tint = 0xff7979;
+        //textvs.fill = "#FFA397";
+        //textvs.stroke = "#DD5257";
+        textvs2 = game.add.bitmapText(220, 280, 'PressStart2P', playerName[0] + " " + playerName[1] + " " + playerName[2] + " " + playerName[3], 50);
+        textvs2.tint = 0x5ffffD;
+        //textvs2.fill = "#5FFFFD";
+        //textvs2.stroke = "#00B9B5";
+        textvs3 = game.add.bitmapText(62, 440, 'PressStart2P', ' press S to choose a letter\npress K to go to next letter', 24);
+        textvs3.tint = 0xff7979;
+        //textvs3.fill = "#FFA397";
+        //textvs3.stroke = "#DD5257";
       }
       if(game.input.keyboard.isDown(Phaser.Keyboard.S) && !pressedS){
         pressedS = true;
@@ -170,12 +147,12 @@ function update () {
       game.stage.backgroundColor = "#222527";
       if(!nameDone){
         if(currentChar === 3){
-          textvs3.text = "press S to choose a letter\npress K to continue";
+          textvs3.text = " press S to choose a letter\n     press K to continue";
         } else{
-          textvs3.text = "press S to choose a letter\npress K to go to next letter";
+          textvs3.text = " press S to choose a letter\npress K to go to next letter";
         }
       } else{
-        textvs3.text = "press S to change name\npress K to go to play";
+        textvs3.text = "   press S to change name\n      press K to play";
       }
       textvs2.text = playerName[0] + " " + playerName[1] + " " + playerName[2] + " " + playerName[3];
       break;
@@ -188,63 +165,36 @@ function update () {
         textvs2.destroy();
         textvs3.destroy();
         //imgOL.destroy();
+        // Text
         imgField = game.add.sprite(0, 0, "field");
-        textvs = game.add.text(90, 40, "HIGH SCORE");
-        textvs.font = "Press Start 2P";
-        textvs.fontSize = 25;
-        textvs.align = "center";
-        textvs.fill = "#FFA397";
-        textvs.stroke = "#DD5257";
-        textvs.strokeThickness = 8;
-        textvs2 = game.add.text(580, 65, player);
-        textvs2.anchor.setTo(0.5, 0.5);
-        textvs2.font = "Press Start 2P";
-        textvs2.fontSize = 25;
-        textvs2.align = "center";
-        textvs2.fill = "#5FFFFD";
-        textvs2.stroke = "#00B9B5";
-        textvs2.strokeThickness = 8;
-        textvs3 = game.add.text(70, 90, "" + hiscore);
-        textvs3.font = "Press Start 2P";
-        textvs3.fontSize = 35;
-        textvs3.align = "center";
-        textvs3.fill = "#C0FF86";
-        textvs3.stroke = "#6BFF00";
-        textvs3.strokeThickness = 8;
-        textvs4 = game.add.text(450, 90, "" + score);
-        textvs4.font = "Press Start 2P";
-        textvs4.fontSize = 35;
-        textvs4.align = "center";
-        textvs4.fill = "#C0FF86";
-        textvs4.stroke = "#6BFF00";
-        textvs4.strokeThickness = 8;
-        donkeyA = game.add.sprite(120, 460, "donkeyA");
+        textvs = game.add.bitmapText(96, 48, 'PressStart2P', 'HIGH SCORE', 24);
+        textvs.tint = 0xff7979;
+        textvs2 = game.add.bitmapText(540, 48, 'PressStart2P', player, 24);
+        textvs2.tint = 0x5ffffd;
+        textvs3 = game.add.bitmapText(96, 96, 'PressStart2P', "" + hiscore);
+        textvs3.tint = 0xc0ff86;
+        //textBM.tint = 0x6bff00;
+        textvs4 = game.add.bitmapText(450, 96, 'PressStart2P', "" + score);
+        textvs4.tint = 0xc0ff86;
+        // Sprites
+        donkeyA = game.add.sprite(120, 460, "donkey");
         donkeyA.anchor.setTo(0.5, 0.5);
         game.physics.arcade.enable(donkeyA);
         donkeyA.body.setSize(10, 50, 45, 30);
-        donkeyB = game.add.sprite(680, 460, "donkeyB");
+        donkeyB = game.add.sprite(680, 460, "donkey");
         donkeyB.anchor.setTo(0.5, 0.5);
+        donkeyB.scale.setTo(-1, 1);
         game.physics.arcade.enable(donkeyB);
         donkeyB.body.setSize(10, 50, -45, 30);
         ball = game.add.sprite(400, 300, "ball");
         ball.anchor.setTo(0.5, 0,5);
         game.physics.arcade.enable(ball);
-        textvs5 = game.add.text(400, 280, "");
-        textvs5.anchor.setTo(0.5, 0.5);
-        textvs5.font = "Press Start 2P";
-        textvs5.fontSize = 50;
-        textvs5.align = "center";
-        textvs5.fill = "#FFA397";
-        textvs5.stroke = "#DD5257";
-        textvs5.strokeThickness = 8;
-        textvs6 = game.add.text(400, 330, "");
-        textvs6.anchor.setTo(0.5, 0.5);
-        textvs6.font = "Press Start 2P";
-        textvs6.fontSize = 25;
-        textvs6.align = "center";
-        textvs6.fill = "#FFA397";
-        textvs6.stroke = "#DD5257";
-        textvs6.strokeThickness = 8;
+        // Game Over text
+        textvs5 = game.add.bitmapText(170, 280, 'PressStart2P', '', 50);
+        textvs5.tint = 0xff7979;
+        textvs6 = game.add.bitmapText(100, 350, 'PressStart2P', '', 26);
+        textvs6.tint = 0xff7979;
+        // Random start direction
         var r = game.rnd.frac();
         if(r < 0.5){
           dir = -1;
@@ -301,11 +251,11 @@ function update () {
         }
       }
       if(donkeyA.y > 300 && kickedA){
-        donkeyA.loadTexture("donkeyA");
+        donkeyA.loadTexture("donkey");
         kickedA = false;
       }
       if(donkeyB.y > 300 && kickedB){
-        donkeyB.loadTexture("donkeyB");
+        donkeyB.loadTexture("donkey");
         kickedB = false;
       }
       ball.body.velocity.x = speed * dir;
